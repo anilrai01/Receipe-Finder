0e20c1d0d4a9aebb014c9d1a3ccbf1df06f94df2
anil.rai01 <anil.rai01041998@gmail.com>
2019-07-03 19:57:49 +0545

    Added Required Dependencies

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 // Global app controller
+
+import test from "./test";
+// console.log(`Imported test is ${test}`);
+// console.log("Type of test is" + typeof(test));
---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+console.log("Imported Test");
+
+export default 12;
